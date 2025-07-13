ea768a448b63971e0bf963117250c0e404a4a634
Kapobajza <kapobajza@gmail.com>
2025-07-13 22:10:27 +0200

    chore: Updated theme colors

---
+++ src/components/Icon/raw/ChevronLeftIcon.tsx
@@ -0,0 +1,16 @@
+import type { SvgProps } from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
+const SvgChevronLeftIcon = (props: SvgProps) => (
+  <Svg
+    fillRule="evenodd"
+    clipRule="evenodd"
+    viewBox="0 0 298 511.93"
+    {...props}
+  >
+    <Path
+      fillRule="nonzero"
+      d="M285.77 441c16.24 16.17 16.32 42.46.15 58.7-16.16 16.24-42.45 16.32-58.69.16l-215-214.47c-16.24-16.16-16.32-42.45-.15-58.69L227.23 12.08c16.24-16.17 42.53-16.09 58.69.15 16.17 16.24 16.09 42.54-.15 58.7l-185.5 185.04z"
+    />
+  </Svg>
+);
+export default SvgChevronLeftIcon;
